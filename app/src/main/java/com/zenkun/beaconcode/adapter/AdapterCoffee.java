84cd915e5db1357ace579fd8101369e3d221d49e
Zen Kun <zenyagami@gmail.com>
2017-02-27 03:40:16 -0600

    added new ViewHolder class for the header in the list

--- app/src/main/java/com/zenkun/beaconcode/adapter/AdapterCoffee.java
+++ app/src/main/java/com/zenkun/beaconcode/adapter/AdapterCoffee.java
@@ -16,27 +16,52 @@ import java.util.List;
  * Created by Zen zenyagami@gmail.com on 24/02/2017.
  */
 
-public class AdapterCoffee extends RecyclerView.Adapter<AdapterCoffee.ViewHolder>  {
+public class AdapterCoffee extends RecyclerView.Adapter<RecyclerView.ViewHolder>  {
     private List<ModelCoffee> list;
+    private static final int HEADER=0;
 
     public AdapterCoffee(List<ModelCoffee> list) {
         this.list = list;
     }
 
     @Override
-    public AdapterCoffee.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-       return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_row_coffe,parent,false));
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        if(viewType==HEADER)
+        {
+           return new ViewHolderHeader(LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_row_header,parent,false));
+        }else
+           return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_row_coffe,parent,false));
     }
 
     @Override
-    public void onBindViewHolder(AdapterCoffee.ViewHolder holder, int position) {
-        ModelCoffee coffee =list.get(position);
-        holder.mPhoto.setImageResource(coffee.resourcePhoto);
-        holder.mTitle.setText(coffee.title);
-        holder.mPromo.setText(coffee.isStarred?R.string.free_coffee : R.string.need_points);
-        holder.mPromo.setTextColor(holder.mPromo.getContext().getResources().getColor(coffee.isStarred?R.color.color_promo_on : R.color.color_promo_off));
-        holder.mStar.setCompoundDrawablesWithIntrinsicBounds(0,0,coffee.isStarred?R.drawable.ic_star: R.drawable.ic_star_off,0);
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
+        if(position!=HEADER)
+        {
+            ModelCoffee coffee =list.get(position);
+            ((ViewHolder)holder).mPhoto.setImageResource(coffee.resourcePhoto);
+            ((ViewHolder)holder).mTitle.setText(coffee.title);
+            ((ViewHolder)holder).mPromo.setText(coffee.isStarred?R.string.free_coffee : R.string.need_points);
+            ((ViewHolder)holder).mPromo.setTextColor(((ViewHolder)holder).mPromo.getContext().getResources().getColor(coffee.isStarred?R.color.color_promo_on : R.color.color_promo_off));
+            ((ViewHolder)holder).mStar.setCompoundDrawablesWithIntrinsicBounds(0,0,coffee.isStarred?R.drawable.ic_star: R.drawable.ic_star_off,0);
+        }
+
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //if position==0 is header, if not is data.
+        return position;
+    }
+
+    public class ViewHolderHeader extends RecyclerView.ViewHolder
+    {
+
+        public ViewHolderHeader(View itemView) {
+            super(itemView);
+        }
     }
 
     @Override
